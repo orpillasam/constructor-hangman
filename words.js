4fabf4e254c9bb1d9ceb736ee85d34f46118bf25
orpillasam <31043722+orpillasam@users.noreply.github.com>
2018-02-23 14:46:02 -0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,17 @@
+module.exports = [
+
+	"Iron Man",
+	"Thor",
+	"Vision",
+	"Spider-Man",
+	"Hulk",
+	"Doctor Strange",
+	"Black Widow",
+	"Hawkeye",
+	"War Machine",
+	"Star Lord",
+	"Gamora",
+	"Drax the Destroyer",
+	"Captain America"
+]
+
